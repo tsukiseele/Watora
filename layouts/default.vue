317ead8f44f29738c91fc59900464c3e8db79733
TsukiSeele <tsukiseele@hotmail.com>
2021-12-03 21:47:43 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -120,7 +120,7 @@ export default {
 <style lang="scss">
 #back {
   position: relative;
-  
+
   &::before {
     content: '';
     position: absolute;
@@ -128,7 +128,7 @@ export default {
     left: 0;
     bottom: 0;
     right: 0;
-    background-image: url(/bg.webp);
+    background-image: url('/bg.webp');
     background-size: cover;
     // background-image: repeating-linear-gradient(45deg, rgba(255, 255, 255, 0.5) 1rem, transparent 1rem 2rem, rgba(255, 255, 255, 0.5) 2rem 3rem);
     // background-color: #8491c3; //#ffec47;
