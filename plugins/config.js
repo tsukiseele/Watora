c8e142c80a10e60a1476348f158c7f61cebf6433
TsukiSeele <tsukiseele@hotmail.com>
2021-12-07 23:25:21 +0800

    Update config.js

--- plugins/config.js
+++ plugins/config.js
@@ -6,9 +6,9 @@ export default {
   // 这里需要把 '_' 下划线替换为 '+'
   token: 'ghp+M39JbvqRn3ipsl8OOZfRFhLPDjR8uO0j7sLs',
   // Github 用户名
-  // username: 'tsukiseele',
+  username: 'tsukiseele',
 
-  username: 'chanshiyucx',
+  // username: 'chanshiyucx',
   // Github 仓库名
   repository: 'blog'
 }
