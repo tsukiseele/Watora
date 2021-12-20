52cbca41e8a25833808cedb67953465e9300d812
tsukiseele <tsukiseele@hotmail.com>
2021-12-20 15:22:31 +0800

    update

--- components/TheAPlayer.vue
+++ components/TheAPlayer.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 client-only
-  aplayer(fixed hideLrc="true" :audio="musics" :lrcType="3" )
+  aplayer(fixed hideLrc="true" crossOrigin=“” :audio="musics" :lrcType="3" )
 </template>
 
 <script>
